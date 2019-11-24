c50e8afc9835120b92894a852f2f4f6dd6457d1a
ColeW-Picaro <colevohs@gmail.com>
2019-11-24 12:12:23 -0500

    this works baby

--- walget.py
+++ walget.py
@@ -1,29 +1,58 @@
-# This in theory works, but there are an unfortunate amount of regexs that match wallhaven's
-# urls and not enough wallpapers to make it work.  It just generates url's that are
-# technically valid, there just aren't wallpapers there :(
+# Pulls down wallhaven home page and extracts image codes of top papes
 import sys
 import os
-import wget
 import exrex
+import re
 import random
+import urllib.request
+import pywal
+
 
 def geturl ():
     #d = exrex.getone ('([a-z]|[0-9]){2}')
-    affix = exrex.getone ('([a-z]|[0-9]){6}')
+    #affix = exrex.getone ('([a-z]|[0-9]){6}')
     #url = "https://w.wallhaven.cc/full/" + d + "/wallhaven-" + affix + ".jpg"
-    url = "https://wallhaven.cc/w/" + affix
+    url = 'https://wallhaven.cc/'
     return url
 
-def wgetfile (url):
-    try:
-        return wget.download (url)
-    except Exception:
-        return 0
-    
-def main ():
-    file = 0
-    while file == 0:
-        file = wgetfile (geturl ())
-    print (url)
+def getCodeUrl (code):
+    return 'https://wallhaven.cc/w/' + code
 
+def getFile (url):
+    req = urllib.request.Request (url, headers={'User-Agent': 'Mozilla/5.0'})
+    html = urllib.request.urlopen (req).read ().decode ('utf-8')
+    return html
+
+# finds all "lg-thumb" spans in html and extracts the 6 character code
+def getImageCodes (html):
+    thumbs = re.findall ('<span class="lg-thumb"><a href="[^"><]*">', html)
+    thumblist = []
+    for thumb in thumbs:
+        thumb = re.sub ('<span class="lg-thumb"><a href="https://wallhaven.cc/w/', '', thumb)
+        thumb = re.sub ('">', '', thumb)
+        thumblist.append (thumb)
+    return thumblist
+
+# Another regex sub to extract full link
+def getImageLink (html):
+    wallpaper = re.search ('<img id="wallpaper" src="https://w.wallhaven.cc/full/.{2}/wallhaven-.{6}.jpg"', html).group ()
+    wallpaper = re.sub ('<img id="wallpaper" src="', '', wallpaper)
+    wallpaper = re.sub ('"', '', wallpaper)
+    return wallpaper
+
+def downloadImage (url):
+    req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"}) 
+    image = urllib.request.urlopen (req).read ()
+    #urllib.request.Request (url, headers={'User-Agent': 'Mozilla/5.0'})
+    return image
+
+def main ():
+    html = getFile (geturl ())
+    codes = getImageCodes (html)
+    html = getFile (getCodeUrl (random.choice (codes)))
+    wallpaper = getImageLink (html)
+    image = downloadImage (wallpaper)
+    f = open ("image.jpg", "wb")
+    f.write (image)
+    f.close ()
 main ()
