d22dfdd6c302c7f368fc6d6c00b6f7599f27cdb0
ColeW-Picaro <colevohs@gmail.com>
2019-11-25 16:30:26 -0500

    wrapped wallhaven things in a class to add support for other websites later

--- walget.py
+++ walget.py
@@ -5,66 +5,79 @@ import re
 import random
 import urllib.request
 
-# Return url with images
-def geturl ():
-    #d = exrex.getone ('([a-z]|[0-9]){2}')
-    #affix = exrex.getone ('([a-z]|[0-9]){6}')
-    #url = "https://w.wallhaven.cc/full/" + d + "/wallhaven-" + affix + ".jpg"
-    url = 'https://wallhaven.cc/'
-    return url
-
-# Append 6 character code on wallhaven url
-def getCodeUrl (code):
-    return 'https://wallhaven.cc/w/' + code
-
+# Global function: Used by all classes
 # pulls html from url
-def getFile (url):
+def getHtml (url):
     req = urllib.request.Request (url, headers={'User-Agent': 'Mozilla/5.0'})
     html = urllib.request.urlopen (req).read ().decode ('utf-8')
     return html
 
-# finds all "lg-thumb" spans in html and extracts the 6 character code
-# sample link
-# https://th.wallhaven.cc/small/6k/6kyk9w.jpg
-# turns into:
-# https://w.wallhaven.cc/full/39/wallhaven-39gogv.jpg
-
-def getImageLinks (html):
-    thumbs = re.findall ('img src="[^"<>]*"', html)
-    thumblist = []
-    for thumb in thumbs:        
-        if "user" in thumb:
-            continue
-        # A lot of unfortuante regex subs to turn a thumbnail link to full size image link
-        # It sucks, but it works 
-        try:
-            thumb = re.sub ('img src=', '', thumb)
-            thumb = re.sub ('/th.', '/w.', thumb)
-            thumb = re.sub ('/small/', '/full/', thumb)
-            thumb = re.sub ('/lg/', "/full/", thumb)
-            code = re.search ('\w{6}.jpg', thumb).group ()
-            thumb = re.sub ('\w{6}.jpg', "wallhaven-" + code, thumb)
-            thumb = re.sub ('"', '', thumb)
-            thumblist.append (thumb)
-        except Exception as e:
-            print (e)
-    return thumblist
-
+# Global function: Used by all classes
 # Downloads the image from url
-def downloadImage (url):
-    req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"}) 
-    image = urllib.request.urlopen (req).read ()
-    #urllib.request.Request (url, headers={'User-Agent': 'Mozilla/5.0'})
-    return image
+def getImage (url):
+    try:
+        req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"}) 
+        image = urllib.request.urlopen (req).read ()
+        #urllib.request.Request (url, headers={'User-Agent': 'Mozilla/5.0'})
+        return image
+    except URLError as e:
+        print (e)
+        return 0
 
+# wallhaven.cc specific behaviors
+class wallhaven ():
+    # ctor to initalize wallhaven homepage as url
+    def __init__ (self):
+        self.url = 'https://wallhaven.cc'
+        self.html = getHtml (self.url)
+        self.thumblist = []
+    # Returns url
+    def geturl (self):
+        return url
+    # Returns thumbnail list
+    def getThumblist (self):
+        return self.thumblist
+    def getHtml (self):
+        return self.html
+    # Sets html of this object
+    def setHtml (self, html):
+        self.html = html
+        return    
+    # finds all img srcs in html and turns it into a full size link
+    # sample link
+    # https://th.wallhaven.cc/small/6k/6kyk9w.jpg
+    # turns into:
+    # https://w.wallhaven.cc/full/39/wallhaven-39gogv.jpg
+    def genThumblist (self):
+        thumbs = re.findall ('img src="[^"<>]*"', self.html)
+        for thumb in thumbs:        
+            if "user" in thumb:
+                continue
+            elif "logo" in thumb:
+                continue
+            # A lot of unfortuante regex subs to turn a thumbnail link to full size image link
+            # It sucks, but it works 
+            try:
+                thumb = re.sub ('img src=', '', thumb)
+                thumb = re.sub ('/th.', '/w.', thumb)
+                thumb = re.sub ('/small/', '/full/', thumb)
+                thumb = re.sub ('/lg/', "/full/", thumb)
+                code = re.search ('\w{6}.jpg', thumb).group ()
+                thumb = re.sub ('\w{6}.jpg', "wallhaven-" + code, thumb)
+                thumb = re.sub ('"', '', thumb)
+                self.thumblist.append (thumb)
+            except Exception as e:
+                print (thumb)                
+        return
+        
 def main ():
-    # Get homepage html
-    html = getFile (geturl ())
-    # Find top image codes
-    links = getImageLinks (html)
-    # Extract Code URL at random
-    # Download the image
-    image = downloadImage (random.choice (links))
+    w = wallhaven ()
+    w.genThumblist ()
+    tl = w.getThumblist ()
+    # Open file and save it
+    image = 0
+    while image == 0:
+        image = getImage (random.choice (tl))
     f = open ("image.jpg", "wb")
     f.write (image)
     f.close ()
--- walget.sh
+++ walget.sh
@@ -3,4 +3,5 @@ image="image.jpg"
 rm $HOME/.cache/wal/schemes/*None_None*.json
 python3 walget.py
 wal -a 85 -i $image
+i3 restart
 rm $image
